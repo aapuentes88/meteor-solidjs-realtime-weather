88d9867265b438182710c5122633243d3c3c1309
Alberto <albaristides@gmail.com>
2024-04-05 00:43:04 -0400

    Including proxy configuration

--- server/main.js
+++ server/main.js
@@ -1,54 +1,67 @@
 import { Meteor } from 'meteor/meteor';
 
-import "/imports/api/weather/collection";
-import "/imports/api/weather/methods";
-import "/imports/api/weather/publications";
-import { Request } from '../server/weather/request'
-import { Monitor } from '../server/weather/monitor'
+import '/imports/api/weather/collection';
+import '/imports/api/weather/methods';
+import '/imports/api/weather/publications';
+import { Request } from '../server/weather/request';
+import { Monitor } from '../server/weather/monitor';
 
 Meteor.startup(() => {
   // Aquí va tu código de inicialización de Meteor, configuraciones, etc.
 
   // Definir el método para inicializar el monitor
   Meteor.methods({
-    async initMonitor( city) {
+    async initMonitor(city) {
       try {
         const { myWeather } = Meteor.settings.monitors;
         await Monitor.prepare();
         // const currentMonitorCustomUrl = `${myWeather.url}weather?lat=${latitude}&lon=${longitude}&appid=${myWeather.apikey}`
         // const forecastMonitorCustomUrl = `${myWeather.url}forecast?lat=${latitude}&lon=${longitude}&appid=${myWeather.apikey}`
-        const currentMonitorCustomUrl = `${myWeather.url}weather?q=${city}&appid=${myWeather.apikey}`
-        const forecastMonitorCustomUrl = `${myWeather.url}forecast?q=${city}&appid=${myWeather.apikey}`
+        const currentMonitorCustomUrl = `${myWeather.url}weather?q=${city}&appid=${myWeather.apikey}`;
+        const forecastMonitorCustomUrl = `${myWeather.url}forecast?q=${city}&appid=${myWeather.apikey}`;
 
-
-        const monitor = async () => {          
-          const request = new Request({ ...myWeather, url:currentMonitorCustomUrl });
+        const monitor = async () => {
+          const request = new Request({
+            ...myWeather,
+            url: currentMonitorCustomUrl,
+          });
           await request.fire();
           await Monitor.addWeather(request.toWeatherDocument());
         };
 
-        const forecast = async () => {          
-          const request = new Request({ ...myWeather, url:forecastMonitorCustomUrl });
+        const forecast = async () => {
+          const request = new Request({
+            ...myWeather,
+            url: forecastMonitorCustomUrl,
+          });
           await request.fire();
           await Monitor.addForecast(request.toForecastDocument());
         };
 
         await monitor();
         await forecast();
-        Monitor.setIdentifier(Meteor.setInterval(monitor, myWeather.interval))
-        Monitor.setForecastIdentifier(Meteor.setInterval(forecast, myWeather.forecastInterval))
+        Monitor.setIdentifier(Meteor.setInterval(monitor, myWeather.interval));
+        Monitor.setForecastIdentifier(
+          Meteor.setInterval(forecast, myWeather.forecastInterval)
+        );
       } catch (error) {
         console.error('Error al iniciar el monitor:', error);
-        throw new Meteor.Error('init-monitor-failure', 'Error al iniciar el monitor');
+        throw new Meteor.Error(
+          'init-monitor-failure',
+          'Error al iniciar el monitor'
+        );
       }
     },
 
     async stopMonitor() {
       try {
-        await Monitor.stop()
-      } catch (error){
-        throw new Meteor.Error('stop-monitor-failure', 'Error al detener el monitor');
+        await Monitor.stop();
+      } catch (error) {
+        throw new Meteor.Error(
+          'stop-monitor-failure',
+          'Error al detener el monitor'
+        );
       }
-    }
+    },
   });
 });
--- server/weather/request.js
+++ server/weather/request.js
@@ -1,40 +1,48 @@
-import { fetch } from 'meteor/fetch'
+import { fetch } from 'meteor/fetch';
+import { HttpsProxyAgent } from 'https-proxy-agent';
 
 export class Request {
-  constructor ({ url, method, timeout }) {
+  constructor({ url, proxy, method, timeout }) {
     // config
-    this.method = method
-    this.url = url; 
-    this.timeout = timeout
- 
-    this.response = null
+    this.method = method;
+    this.url = url;
+    this.proxy = proxy;
+    this.timeout = timeout;
 
-    this.error = null
-    this.hasTimedOut = null
+    this.response = null;
+
+    this.error = null;
+    this.hasTimedOut = null;
   }
 
-  async fire () {
-
+  async fire() {
     try {
-      const response = await fetch(this.url, {
-        method: this.method
-        // mode: 'cors', 
-      });
+      const options = this.proxy
+        ? {
+            method: this.method,
+            mode: 'cors',
+            agent: new HttpsProxyAgent(this.proxy), // Configurar el proxy aquí
+          }
+        : {
+            method: this.method,
+            mode: 'cors',
+          };
+
+      const response = await fetch(this.url, options);
 
       if (!response.ok) {
-        console.log(`Error: ${response.status} - ${response.statusText}`)
+        console.log(`Error: ${response.status} - ${response.statusText}`);
         throw new Error(`Error: ${response.status} - ${response.statusText}`);
       }
 
       this.response = await response.json(); // Suponiendo que la respuesta es JSON
-
     } catch (error) {
+      console.log(`Error: ${response.status} - ${response.statusText}`);
       this.error = error.toString();
     }
   }
 
-  toWeatherDocument () {
-
+  toWeatherDocument() {
     const document = {
       coordinates: this.response?.coord,
       createdAt: new Date(),
@@ -46,32 +54,27 @@ export class Request {
       clouds: this.response?.clouds,
       visibility: this.response?.visibility,
       hasTimedOut: !!this.hasTimedOut,
-      error: this.error
-    }    
+      error: this.error,
+    };
 
-    document.main.temp = (document.main.temp - 273.15)
-                         .toFixed(2)
-    document.main.feels_like = (document.main.feels_like - 273.15)
-                               .toFixed(2)
-    document.main.temp_min = (document.main.temp_min - 273.15)
-                             .toFixed(2)
-    document.main.temp_max =  (document.main.temp_max - 273.15)
-                              .toFixed(2)
-   
-    return document
+    document.main.temp = (document.main.temp - 273.15).toFixed(2);
+    document.main.feels_like = (document.main.feels_like - 273.15).toFixed(2);
+    document.main.temp_min = (document.main.temp_min - 273.15).toFixed(2);
+    document.main.temp_max = (document.main.temp_max - 273.15).toFixed(2);
+
+    return document;
   }
 
-  toForecastDocument () {
-
+  toForecastDocument() {
     const document = {
       coordinates: this.response?.city.coord,
       createdAt: new Date(),
       city: this.response?.city.name,
-      list: this.response?.list.sort((a, b) => a.dt - b.dt).slice(0,9),
+      list: this.response?.list.sort((a, b) => a.dt - b.dt).slice(0, 9),
       hasTimedOut: !!this.hasTimedOut,
-      error: this.error
-    }    
- 
-    return document
+      error: this.error,
+    };
+
+    return document;
   }
 }
